5176978ee10b84217f4f65cc769ddba0f8af44b6
Qaissumshahaab <qaissumshahaab@gmail.com>
2026-07-26 16:06:10 +0500

    wrote Dockerfile and compose.yaml for nginx deployement

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+  
+      <> this is page at path / that i will deploy on nginx using dockerfile and compose file and bash script</>
+  )
+}
+
+export default App
