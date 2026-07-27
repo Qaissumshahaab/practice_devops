3ad1416aaa93d74d0ff9c4349945a54ecd7ec97d
Qaissumshahaab <qaissumshahaab@gmail.com>
2026-07-27 18:41:32 +0500

    modified App.jsx
    modified App.jsx so i can run deploy.sh in self-hosted runner again through push on github

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
   
-      <> this is page at path / that i will deploy on nginx using dockerfile and compose file and bash script</>
+      <> this is page at path / that i will deploy on nginx using dockerfile and compose file and bash script ,i am using nginx to serve files and deploy.sh to run compose file</>
   )
 }
 
